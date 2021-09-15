81f996f3204c2fa1742186507b6cfe4761e09368
Diego Ortiz <djkimer@gmail.com>
2021-09-15 08:06:55 -0500

    Namespaces Clases Campos Propiedades Y Metodos
    Namespaces Clases Campos Propiedades Y Metodos

--- ProgrammingCSharp/ProgrammingCSharp/Program.cs
+++ ProgrammingCSharp/ProgrammingCSharp/Program.cs
@@ -8,67 +8,45 @@ namespace ProgrammingCSharp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Person person1 = new Person();
+            Person person2 = new Person("Diego");
+            Person person3 = new Person("Luigi", "Sanchez");
 
-            //variables
-            //[datatype] [identifier];
-            string customer;
-            //[datatype] [identifier] = [value];
-            string customeR = "Diego Ortiz";
-            //Camel
-            string customerFirstName = "";
-            //constante
-            const string name = "Diego";
-
-            //Tipos númericos
-            byte myByte = 255; //8 bits
-            Byte myByteSystem = 255;  //8 bits
-            short myShort = -32768; //16 bits
-            int myInt; //32 bits
-            long myLong; //64 bits
-
-            //Númericos de punto flotante
-            float myFloat;
-            double myDouble;
-            decimal myDecimal;
-
-            //Boolean
-            Boolean myBoolean;
-
-            //Char
-            char myChar = 'c';
-
-            //Operadores aritmeticos
-
-            int x = 5 % 2;
-
-            //Operadores de comparación
-            //< > <= >=
-
-            //Igualdad
-            //== !=
-
-            //Operadores logicos 
-            // ! && ||
-
-            //Conversiones
-            //1.Conversion Implicita, de tipos enteros pequeños a enteros más grandes.
-            short myShort255_ = 255;
-            int myInt16Bits = myShort255_;
-
-            //2.Requieren casteo. eje. decimal to float (float).
-            decimal myDecimal2 = 3;
-            float myFloat2 = (float)myDecimal2;
-
-            //3.Conversiones definidas por usuario - Conversión dada por reglas logicas.
-
-            //4.Conversiones con clases del asisitente -  utilizada para tipos no compatibles cómo enteros a otros objetos
-            int intParsed = int.Parse("5");
-            int intConverted = Convert.ToInt32("5");  
-            
-            //TODO: finalizar clase.
-
+            person1.FirstName = "Juan Camilo";
+            Console.WriteLine("Person1: "+ person1.FirstName);
+            Console.WriteLine("Person2: "+ person2.GetFullName());
+            Console.WriteLine("Person3: "+ person3.GetFullName());
 
 
         }
+    }
+
+
+    class Person
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Email { get; set; }
+
+        public byte Edad { get; set; }
+
+        public Person()
+        {
+
+        }
+
+        public Person(string firstName, string lastName = "NN")
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+        public string GetFullName()
+        {
+            return FirstName+" "+LastName;
+        }
+        
     }
 }
---
+++ ProgrammingCSharp/ProgrammingCSharp/VariablesConstantesYTipos.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProgrammingCSharp
+{
+    class VariablesConstantesYTipos
+    {
+        public void MethodName()
+        {
+            //variables
+            //[datatype] [identifier];
+            string customer;
+            //[datatype] [identifier] = [value];
+            string customeR = "Diego Ortiz";
+            //Camel
+            string customerFirstName = "";
+            //constante
+            const string name = "Diego";
+
+            //Tipos númericos
+            byte myByte = 255; //8 bits
+            Byte myByteSystem = 255;  //8 bits
+            short myShort = -32768; //16 bits
+            int myInt; //32 bits
+            long myLong; //64 bits
+
+            //Númericos de punto flotante
+            float myFloat;
+            double myDouble;
+            decimal myDecimal;
+
+            //Boolean
+            Boolean myBoolean;
+
+            //Char
+            char myChar = 'c';
+
+            //Operadores aritmeticos
+
+            int x = 5 % 2;
+
+            //Operadores de comparación
+            //< > <= >=
+
+            //Igualdad
+            //== !=
+
+            //Operadores logicos 
+            // ! && ||
+
+            //Conversiones
+            //1.Conversion Implicita, de tipos enteros pequeños a enteros más grandes.
+            short myShort255_ = 255;
+            int myInt16Bits = myShort255_;
+
+            //2.Requieren casteo. eje. decimal to float (float).
+            decimal myDecimal2 = 3;
+            float myFloat2 = (float)myDecimal2;
+
+            //3.Conversiones definidas por usuario - Conversión dada por reglas logicas.
+
+            //4.Conversiones con clases del asisitente -  utilizada para tipos no compatibles cómo enteros a otros objetos
+            int intParsed = int.Parse("5");
+            int intConverted = Convert.ToInt32("5");
+
+            //TODO: finalizar clase.
+        }
+
+    }
+}
